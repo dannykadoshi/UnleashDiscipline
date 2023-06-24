a845026712ae90a6c3c6e9d3e60a6fc5631b2804
dannykadoshi <>
2023-06-24 15:08:55 +0000

    Create contact.js, thankyou.js, Add button event listeners

---
+++ assets/js/contact.js
@@ -0,0 +1,16 @@
+/** 
+ * Event listener to the Back home button located at the contact.html page
+ * Clicking the button will trigger the event listener to navigate back to the home page
+ */
+// Get the Back Home button element
+const backHomeButton = document.getElementById('contact-back-btn');
+
+// Add click event listener to the Back Home button
+backHomeButton.addEventListener('click', navigateBackHome);
+
+// Function to navigate back to the home page
+function navigateBackHome(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    window.location.href = 'index.html'; // Replace 'index.html' with the actual URL of your home page
+}
+
---
+++ assets/js/thankyou.js
@@ -0,0 +1,12 @@
+  /** 
+   * Event listener registration for the close button at the thankyou.html page
+   */
+  document.addEventListener('DOMContentLoaded', function() {
+    // Event listener registration for the close button
+    const closeButton = document.getElementById("thanks-close-btn");
+    if (closeButton) {
+      closeButton.addEventListener("click", function() {
+        window.location.href = "index.html";
+      });
+    }
+  });
